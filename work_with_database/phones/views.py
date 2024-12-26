4ced413b983292ee110f73e29c30f1dbf01c73e1
Elena Lopatina <lokila667@gmail.com>
2024-12-26 19:13:00 +0400

    Создан view файл

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect
+from Phone.models import Phone
 
 
 def index(request):
-    return redirect('catalog')
+    return redirect('show_catalog')
 
 
 def show_catalog(request):
-    template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    phones = Phone.objects.all()
+    sort_by = request.GET.get('sort')
+    if sort_by == 'name':
+        phones = phones.order_by('name')
+    elif sort_by == 'min_price':
+        phones = phones.order_by('price')
+    elif sort_by == 'max_price':
+        phones = phones.order_by('-price')
+
+    context = {'phones': phones}
+    return render(request, 'catalog.html', context)
 
 
 def show_product(request, slug):
